ae2c5d60803d8ebc3eae8d4ab61366eedfce2e8c
rodr15 <anthony.rodriguezf11@gmail.com>
2023-09-17 14:49:32 -0500

    (add) anime use case test

---
+++ test/src/domain/usecases/anime.usecase_test.dart
@@ -0,0 +1,56 @@
+import 'package:anime/src/domain/exceptions/request.exception.dart';
+import 'package:anime/src/domain/models/anime.model.dart';
+import 'package:anime/src/domain/repositories/anime.repository.dart';
+import 'package:anime/src/domain/usecases/anime.usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'anime.usecase_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AnimeRepository>()])
+void main() {
+  late AnimeUseCase animeUseCase;
+  late AnimeRepository animeRepository;
+  setUp(() {
+    animeRepository = MockAnimeRepository();
+    animeUseCase = AnimeUseCase(animeRepository);
+  });
+
+  group('👀 AnimeUseCase ->', () {
+    group('getAnimeByWord -> ', () {
+      test('[Throw] Exception', () {
+        when(animeRepository.getAnimesByWord(''))
+            .thenAnswer((_) async => throw RequestException.badRequest());
+
+        expect(() => animeUseCase.getAnimesByWord(''), throwsException);
+      });
+      test('[Return] animes', () async {
+        when(animeRepository.getAnimesByWord(''))
+            .thenAnswer((_) async => <Anime>[]);
+
+        final animes = await animeUseCase.getAnimesByWord('');
+
+        expect(animes, []);
+        expect(animes.runtimeType, List<Anime>);
+      });
+    });
+    group('getSeasonAnime -> ', () {
+      test('[Throw] Exception', () {
+        when(animeRepository.getSeasonPopularAnime())
+            .thenAnswer((_) async => throw RequestException.badRequest());
+
+        expect(() => animeUseCase.getSeasonAnime(), throwsException);
+      });
+      test('[Return] animes', () async {
+        when(animeRepository.getSeasonPopularAnime())
+            .thenAnswer((_) async => <Anime>[]);
+
+        final animes = await animeUseCase.getSeasonAnime();
+
+        expect(animes, []);
+        expect(animes.runtimeType, List<Anime>);
+      });
+    });
+  });
+}
