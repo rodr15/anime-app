de8258442b1f864e95ea20ccfd950612aaac2ea0
rodr15 <anthony.rodriguezf11@gmail.com>
2023-09-21 12:10:11 -0500

    (fix) search bloc now the search bloc is provided from nav bar

--- lib/src/presentation/common/widgets/scaffold_nav_bar.widget.dart
+++ lib/src/presentation/common/widgets/scaffold_nav_bar.widget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../common/extensions/string.dart';
+import '../../../configuration/di/dependency_injection.dart';
+import '../../features/search/controller/search_bloc.bloc.dart';
 
 /// Builds the "shell" for the app by building a Scaffold with a
 /// BottomNavigationBar, where [child] is placed in the body of the Scaffold.
@@ -18,25 +21,28 @@ class ScaffoldWithNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: navigationShell,
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.home),
-            label: S.of(context)!.popularPageTitle.capitalize(),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.list),
-            label: S.of(context)!.myListPageTitle.capitalize(),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.person),
-            label: S.of(context)!.settingsTitle.capitalize(),
-          ),
-        ],
-        currentIndex: navigationShell.currentIndex,
-        onTap: (int index) => _onTap(context, index),
+    return BlocProvider.value(
+      value: di<SearchBloc>(),
+      child: Scaffold(
+        body: navigationShell,
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.home),
+              label: S.of(context)!.popularPageTitle.capitalize(),
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.list),
+              label: S.of(context)!.myListPageTitle.capitalize(),
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.person),
+              label: S.of(context)!.settingsTitle.capitalize(),
+            ),
+          ],
+          currentIndex: navigationShell.currentIndex,
+          onTap: (int index) => _onTap(context, index),
+        ),
       ),
     );
   }
--- lib/src/presentation/features/search/search.app_bar.widget.dart
+++ lib/src/presentation/features/search/search.app_bar.widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../configuration/di/dependency_injection.dart';
 import 'controller/search_bloc.bloc.dart';
 import 'widgets/search_search_delegate.dart';
 
@@ -10,21 +9,18 @@ class SearchAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: di<SearchBloc>(),
-      child: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: SearchSearchDelegate(context.read<SearchBloc>()),
-              );
-            },
-          )
-        ],
-      ),
+    return AppBar(
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {
+            showSearch(
+              context: context,
+              delegate: SearchSearchDelegate(context.read<SearchBloc>()),
+            );
+          },
+        )
+      ],
     );
   }
 
